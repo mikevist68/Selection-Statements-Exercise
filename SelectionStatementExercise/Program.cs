71d7aa0bde0b3a1396373d4fae6d5a10e633db59
Michael Vistocci <mikevist68@yahoo.com>
2023-05-16 13:55:44 -0400

    completed

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            GuessingGame();
+
         }
+
+
+        public static void GuessingGame()
+        {
+
+
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+
+            Console.WriteLine("Try to guess a number");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+        
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"Too low!!!\nYour guess: {userInput}");
+            }
+
+
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"Too high \nYour guess: {userInput}");
+            }
+
+            else
+
+            {
+                Console.WriteLine("You guessed it yay.");
+            }
+
+
+        }
+
+
+
+        
     }
 }
